3d0f58a04fa561aab953f851df8ecf3dff5911ee
pengyejun <1656523732@qq.com>
2024-07-31 17:24:31 +0800

    中标交底项目表类型、商机项目(不含境外商机项目) 表初始化

--- GDCMasterDataReceiveApi.Domain/Models/BusinessNoCpportunity.cs
+++ GDCMasterDataReceiveApi.Domain/Models/BusinessNoCpportunity.cs
@@ -8,6 +8,75 @@ namespace GDCMasterDataReceiveApi.Domain.Models
     [SugarTable("t_businessnocpportunity", IsDisabledDelete = true)]
     public class BusinessNoCpportunity : BaseEntity<long>
     {
-
+        /// <summary>
+        /// 商机项目主数据编码:新增项目由主数据系统生成并返回主数据编码，修改时必填
+        /// </summary>
+        [SugarColumn(Length = 8, ColumnName = "BusinessOpportunityProjectMDCode")]
+        public string ZBOP { get; set; }
+        /// <summary>
+        /// 商机项目名称:商机项目的中文名称，该字段作为境内和港澳台商机项目的唯一标识
+        /// </summary>
+        [SugarColumn(Length = 200, ColumnName = "BusinessOpportunityProjectName")]
+        public string ZBOPN { get; set; }
+        /// <summary>
+        /// 商机项目外文名称:商机项目当地官方语言名称，该字段作为境外（不包括港澳台）商机项目的唯一标识
+        /// </summary>
+        [SugarColumn(Length = 500, ColumnName = "ForeignNameOfBusinessOpportunityProject")]
+        public string ZBOPN_EN { get; set; }
+        /// <summary>
+        /// 项目类型:按照字典表进行选择
+        /// </summary>
+        [SugarColumn(Length = 6, ColumnName = "ProjectType")]
+        public string ZPROJTYPE { get; set; }
+        /// <summary>
+        /// 中交项目业务分类:按照字典表进行选择，涉及多个业务分类的商机项目，其属性应优先归入合同额占比最大的业务分类，若合同额占比相同，应归入实施难度最大的业务分类
+        /// </summary>
+        [SugarColumn(Length = 5, ColumnName = "BusinessClassificationOfCCCCProjects")]
+        public string ZCPBC { get; set; }
+        /// <summary>
+        /// 国家/地区:商机项目所在的国家/地区
+        /// </summary>
+        [SugarColumn(Length = 3, ColumnName = "Country")]
+        public string ZZCOUNTRY { get; set; }
+        /// <summary>
+        /// 项目所在地:参照项目主数据标准要求填写，明确到市级地点，境内项目必填
+        /// </summary>
+        [SugarColumn(Length = 6, ColumnName = "ProjectLocation")]
+        public string ZPROJLOC { get; set; }
+        /// <summary>
+        /// 开始跟踪日期:填写首次跟踪的日期
+        /// </summary>
+        [SugarColumn(Length = 8, ColumnName = "StartTrackingDate")]
+        public string ZSFOLDATE { get; set; }
+        /// <summary>
+        ///跟踪单位:填写跟踪单位的机构主数据编码
+        /// </summary>
+        [SugarColumn(Length = 20, ColumnName = "TrackingUnit")]
+        public string ZORG { get; set; }
+        /// <summary>
+        /// 所属二级单位
+        /// </summary>
+        [SugarColumn(Length = 20, ColumnName = "SecondUnit")]
+        public string Z2NDORG { get; set; }
+        /// <summary>
+        /// 状态: 数据是否有效的标识: 有效：1无效：0
+        /// </summary>
+        [SugarColumn(Length = 1, ColumnName = "State")]
+        public string ZSTATE { get; set; }
+        /// <summary>
+        /// 资质单位
+        /// </summary>
+        [SugarColumn(Length = 20, ColumnName = "QualificationUnit")]
+        public string ZORG_QUAL { get; set; }
+        /// <summary>
+        /// 计税方式
+        /// </summary>
+        [SugarColumn(Length = 1, ColumnName = "TaxationMethod")]
+        public string ZTAXMETHOD { get; set; }
+        /// <summary>
+        /// 参与单位:填写参与部门的行政机构主数据编码，可多值，用英文逗号隔开.
+        /// </summary>
+        [SugarColumn(Length = 300, ColumnName = "ParticipatingUnits")]
+        public string? ZCY2NDORG { get; set; }
     }
 }
---
+++ GDCMasterDataReceiveApi.Domain/Models/TypeOfBidDisclosureProjectTable.cs
@@ -0,0 +1,62 @@
+﻿using SqlSugar;
+
+namespace GDCMasterDataReceiveApi.Domain.Models
+{
+    /// <summary>
+    /// 中标交底项目表类型
+    /// </summary>
+    [SugarTable("t_typeofbiddisclosureprojecttable", IsDisabledDelete = true)]
+    public class TypeOfBidDisclosureProjectTable : BaseEntity<long>
+    {
+        /// <summary>
+        /// 中标交底项目编码:格式：BO000001-01
+        /// </summary>
+        [SugarColumn(Length = 11, ColumnName = "BidDisclosureProjectCode")]
+        public string ZAWARDP { get; set; }
+        /// <summary>
+        /// 交底项目二级单位
+        /// </summary>
+        [SugarColumn(Length = 20, ColumnName = "DisclosureProjectToSecondaryUnits")]
+        public string ZAP2NDORG { get; set; }
+        /// <summary>
+        /// 中标资质单位
+        /// </summary>
+        [SugarColumn(Length = 20, ColumnName = "WinningBidderQualificationUnit")]
+        public string ZAWARDORG { get; set; }
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        [SugarColumn(Length = 200, ColumnName = "ProjectName")]
+        public string ZAWARDPN { get; set; }
+        /// <summary>
+        /// 项目外文名称:境外项目必填
+        /// </summary>
+        [SugarColumn(Length = 500, ColumnName = "ProjectForeignName")]
+        public string ZAWPN_EN { get; set; }
+        /// <summary>
+        /// 中交项目业务分类
+        /// </summary>
+        [SugarColumn(Length = 5, ColumnName = "BusinessClassificationOfCCCCProjects")]
+        public string ZSCPBC { get; set; }
+        /// <summary>
+        /// 项目所在地:境内项目必填
+        /// </summary>
+        [SugarColumn(Length = 6, ColumnName = "ProjectLocation")]
+        public string ZSPROJLOC { get; set; }
+        /// <summary>
+        /// 项目类型
+        /// </summary>
+        [SugarColumn(Length = 6, ColumnName = "ProjectType")]
+        public string ZSPROTYPE { get; set; }
+        /// <summary>
+        /// 是否联合体总项目:0-否，1-是
+        /// </summary>
+        [SugarColumn(Length = 1, ColumnName = "IsItAJointVentureProject")]
+        public string ZSZCONTPRO { get; set; }
+        /// <summary>
+        /// 数据状态:0-停用，1-启用
+        /// </summary>
+        [SugarColumn(Length = 1, ColumnName = "DataStatus")]
+        public string ZSSTATE { get; set; }
+    }
+}
