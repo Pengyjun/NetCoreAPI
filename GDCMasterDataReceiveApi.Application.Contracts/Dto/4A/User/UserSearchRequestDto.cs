a63bdebbac18e28be87b1614f245e98ca00238bd
pengyejun <1656523732@qq.com>
2024-09-13 17:02:32 +0800

    1

--- GDCMasterDataReceiveApi.Application.Contracts/Dto/4A/User/UserSearchRequestDto.cs
+++ GDCMasterDataReceiveApi.Application.Contracts/Dto/4A/User/UserSearchRequestDto.cs
@@ -6,9 +6,18 @@
     public class UserSearchRequestDto : BaseRequestDto
     {
         /// <summary>
-        /// 拼接的 条件 AES加密文本
+        /// 筛选条件json
         /// </summary>
-        //public string? EncryptedText {  get; set; }
-        public List<string>? FilterConditions { get; set; }
+        public string? FilterConditionJson { get; set; }
+    }
+    /// <summary>
+    /// 用户条件反序列化实例
+    /// </summary>
+    public class DeserializeObjectUser
+    {
+        /// <summary>
+        /// 用户ids（首页下钻）
+        /// </summary>
+        public List<string>? Ids { get; set; }
     }
 }
