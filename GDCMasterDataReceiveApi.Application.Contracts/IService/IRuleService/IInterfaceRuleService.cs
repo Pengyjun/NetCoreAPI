1d9e0ba9aa27c0b8755c0b24c0481b51583a7d8a
pengyejun <1656523732@qq.com>
2024-09-18 10:40:25 +0800

    1

---
+++ GDCMasterDataReceiveApi.Application.Contracts/IService/IRuleService/IInterfaceRuleService.cs
@@ -0,0 +1,19 @@
+﻿using GDCMasterDataReceiveApi.Application.Contracts.Dto;
+using GDCMasterDataReceiveApi.Domain.Shared;
+using GDCMasterDataReceiveApi.Domain.Shared.Annotation;
+
+namespace GDCMasterDataReceiveApi.Application.Contracts.IService.IRuleService
+{
+    /// <summary>
+    /// 接口规则列表
+    /// </summary>
+    [DependencyInjection]
+    public interface IInterfaceRuleService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<ResponseAjaxResult<List<InterfaceNamesResponseDto>>> GetInterfaceNamesAsync();
+    }
+}
