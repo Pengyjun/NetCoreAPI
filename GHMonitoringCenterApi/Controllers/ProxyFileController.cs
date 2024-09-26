d798406f5072a2a71926bf6609379e52190848c4
xuyuguang <1656523733@qq.com>
2024-09-26 13:11:59 +0800

    增加图片预览接口

--- GHMonitoringCenterApi/Controllers/ProxyFileController.cs
+++ GHMonitoringCenterApi/Controllers/ProxyFileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UtilsSharp;
 
 namespace GHMonitoringCenterApi.Controllers
 {
@@ -7,5 +8,18 @@ namespace GHMonitoringCenterApi.Controllers
     [ApiController]
     public class ProxyFileController : BaseController
     {
+
+        /// <summary>
+        /// 图片预览
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        [HttpGet("ImagePrview")]
+        public async Task<IActionResult> ImagePrviewAsync(string fileName)
+        {
+            var filePath=AppsettingsHelper.GetValue("UpdateItem:SavePath")+ fileName;
+            var imageFileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            return File(imageFileStream, "image/jpeg");
+        }
     }
 }
