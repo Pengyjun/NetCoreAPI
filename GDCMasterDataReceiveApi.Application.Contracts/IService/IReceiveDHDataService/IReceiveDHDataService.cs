83ca9c659cfcbe534be4e4d6fb060d2f8b7e6ba1
pengyejun <1656523732@qq.com>
2024-10-10 08:58:10 +0800

    接收DH相关数据初始化

---
+++ GDCMasterDataReceiveApi.Application.Contracts/IService/IReceiveDHDataService/IReceiveDHDataService.cs
@@ -0,0 +1,13 @@
+﻿using GDCMasterDataReceiveApi.Domain.Shared.Annotation;
+
+namespace GDCMasterDataReceiveApi.Application.Contracts.IService.IReceiveDHDataService
+{
+    /// <summary>
+    /// 接收DH相关数据接口
+    /// </summary>
+    [DependencyInjection]
+    public interface IReceiveDHDataService
+    {
+
+    }
+}
---
+++ GDCMasterDataReceiveApi/Controller/ReceiveDHDataController.cs
@@ -0,0 +1,24 @@
+﻿using GDCMasterDataReceiveApi.Application.Contracts.IService.IReceiveDHDataService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GDCMasterDataReceiveApi.Controller
+{
+    /// <summary>
+    /// 对接DH相关数据
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiveDHDataController : BaseController
+    {
+        private readonly IReceiveDHDataService _receiveDHDataService;
+        /// <summary>
+        /// 
+        /// </summary>
+        public ReceiveDHDataController(IReceiveDHDataService receiveDHDataService)
+        {
+            this._receiveDHDataService = receiveDHDataService;
+        }
+
+
+    }
+}
