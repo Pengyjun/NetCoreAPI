a370c5dde8bd950fdd14597d6578ecf967cfc17b
xuyuguang <1656523733@qq.com>
2024-10-14 16:49:30 +0800

    修改表的字段非空验证

--- GDCMasterDataReceiveApi.Domain/Models/DeviceClassAttribute.cs
+++ GDCMasterDataReceiveApi.Domain/Models/DeviceClassAttribute.cs
@@ -13,25 +13,25 @@ namespace GDCMasterDataReceiveApi.Domain.Models
         /// 分类编码:分类的唯一性编码
         /// </summary>
         [SugarColumn(Length = 100, ColumnName = "ClassCode")]
-        public string ZCLASS { get; set; }
+        public string? ZCLASS { get; set; }
         /// <summary>
         /// 属性编码:属性唯一代码
         /// 10
         /// </summary>
         [SugarColumn(Length = 100, ColumnName = "Code")]
-        public string ZATTRCODE { get; set; }
+        public string? ZATTRCODE { get; set; }
         /// <summary>
         /// 属性名称
         /// 100
         /// </summary>
         [SugarColumn(Length = 200, ColumnName = "Name")]
-        public string ZATTRNAME { get; set; }
+        public string? ZATTRNAME { get; set; }
         /// <summary>
         /// 属性计量单位
         /// 50
         /// </summary>
         [SugarColumn(Length = 100, ColumnName = "Unit")]
-        public string ZATTRUNIT { get; set; }
+        public string? ZATTRUNIT { get; set; }
         /// <summary>
         /// 备注
         /// 300
