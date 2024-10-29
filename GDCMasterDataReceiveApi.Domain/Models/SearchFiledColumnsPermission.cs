204ccfa4b5d100c904211b5ca7e9fd7565a78c8c
pengyejun <1656523732@qq.com>
2024-10-29 09:30:29 +0800

    字段列展示相关

---
+++ GDCMasterDataReceiveApi.Domain/Models/SearchFiledColumnsPermission.cs
@@ -0,0 +1,26 @@
+﻿using SqlSugar;
+
+namespace GDCMasterDataReceiveApi.Domain.Models
+{
+    /// <summary>
+    /// 列表字段展示权限
+    /// </summary>
+    [SugarTable("t_searchfiledcolumnspermission", IsDisabledDelete = true)]
+    public class SearchFiledColumnsPermission : BaseEntity<long>
+    {
+        /// <summary>
+        /// 字段列 字符串json
+        /// </summary>
+        [SugarColumn(Length = 5000)]
+        public string? FiledColumns { get; set; }
+        /// <summary>
+        /// 租户  / 后续根据登录用户保存  目前暂不处理
+        /// </summary>
+        [SugarColumn(Length = 2000)]
+        public string? Tenant { get; set; }
+        /// <summary>
+        /// 接口列表id
+        /// </summary>
+        public string InterfaceId { get; set; }
+    }
+}
