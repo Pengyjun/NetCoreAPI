465dfecd71430cba5bc58592e590b59eac1b1399
xuyuguang <1656523733@qq.com>
2025-02-19 10:14:10 +0800

    1

--- GHMonitoringCenterApi.Application/Service/JjtSendMessage/JjtSendMessageService.cs
+++ GHMonitoringCenterApi.Application/Service/JjtSendMessage/JjtSendMessageService.cs
@@ -2966,25 +2966,25 @@ namespace GHMonitoringCenterApi.Application.Service.JjtSendMessage
             var npids=drData.Select(x => x.ProjectId).Distinct().ToList();
             var np = improjects.Where(x => npids.Contains(x.ProjectId.Value)).ToList();
 
-            foreach (var item in np)
-            {
-                var islow = drData.Where(x => x.ProjectId == item.ProjectId && x.DateDay == currentTimeInt && x.IsLow == 0).FirstOrDefault();
-                if (islow != null)
-                {
-                    imp.Add(new ImpProjectWarning
-                    {
-                        IsLow = islow.IsLow,
-                        DayAmount = drData.FirstOrDefault(x => x.ProjectId == item.ProjectId)?.DayActualProductionAmount / 10000,
-                        DeviationWarning = drData.FirstOrDefault(x => x.ProjectId == item.ProjectId)?.DeviationWarning,
-                        ProjectName = item.ProjectName
-                    });
-                }
+            //foreach (var item in np)
+            //{
+            //    var islow = drData.Where(x => x.ProjectId == item.ProjectId && x.DateDay == currentTimeInt && x.IsLow == 0).FirstOrDefault();
+            //    if (islow != null)
+            //    {
+            //        imp.Add(new ImpProjectWarning
+            //        {
+            //            IsLow = islow.IsLow,
+            //            DayAmount = drData.FirstOrDefault(x => x.ProjectId == item.ProjectId)?.DayActualProductionAmount / 10000,
+            //            DeviationWarning = drData.FirstOrDefault(x => x.ProjectId == item.ProjectId)?.DeviationWarning,
+            //            ProjectName = item.ProjectName
+            //        });
+            //    }
 
-            }
-            foreach (var item in imp)
-            {
-                item.DayAmount = item.DayAmount == 0 || string.IsNullOrWhiteSpace(item.DayAmount.ToString()) ? 0M : Math.Round(item.DayAmount.Value, 2);
-            }
+            //}
+            //foreach (var item in imp)
+            //{
+            //    item.DayAmount = item.DayAmount == 0 || string.IsNullOrWhiteSpace(item.DayAmount.ToString()) ? 0M : Math.Round(item.DayAmount.Value, 2);
+            //}
             #endregion
 
 
