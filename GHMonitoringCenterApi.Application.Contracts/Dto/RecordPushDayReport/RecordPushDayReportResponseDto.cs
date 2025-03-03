c1b3a37182972750d4859e7362761343574c76ec
袁佳有 <manto66666@163.com>
2025-03-03 18:40:29 +0800

    公司滚动计划数据调整，生产日报推送支持历史数据导出

---
+++ GHMonitoringCenterApi.Application.Contracts/Dto/RecordPushDayReport/RecordPushDayReportResponseDto.cs
@@ -0,0 +1,26 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GHMonitoringCenterApi.Application.Contracts.Dto.RecordPushDayReport
+{
+    /// <summary>
+    /// 每天推送日报结果记录
+    /// </summary>
+    public class RecordPushDayReportResponseDto
+    {
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public int DateDay { get; set; }
+        /// <summary>
+        /// json结果集
+        /// </summary>
+        public string? Json { get; set; }
+
+        public int Index { get; set; }
+    }
+}
---
+++ GHMonitoringCenterApi/Controllers/RecordPushDayReport/RecordPushDayReportController.cs
@@ -0,0 +1,45 @@
+﻿
+using GHMonitoringCenterApi.Domain.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using GHMonitoringCenterApi.Application.Contracts.IService.Push;
+using GHMonitoringCenterApi.Filters;
+using GHMonitoringCenterApi.Application.Contracts.IService.RecordPushDayReport;
+using GHMonitoringCenterApi.Application.Contracts.Dto.RecordPushDayReport;
+
+namespace GHMonitoringCenterApi.Controllers.RecordPushDayReport;
+
+/// <summary>
+///  每天推送日报结果记录
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+[ThreadLockFilter]
+public class RecordPushDayReportController : BaseController
+{
+    /// <summary>
+    /// 推送日报结果记录业务层
+    /// </summary>
+
+    private readonly IRecordPushDayReportService _recordPushDayReportService;
+
+    /// <summary>
+    /// 推送日报结果记录
+    /// </summary>
+    public RecordPushDayReportController(IRecordPushDayReportService recordPushDayReportService)
+    {
+        _recordPushDayReportService = recordPushDayReportService;
+    }
+    /// <summary>
+    ///获取每天推送日报结果
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("GetRecordPushDayReportList")]
+    public async Task<ResponseAjaxResult<List<RecordPushDayReportResponseDto>>> GetRecordPushDayReportListAsync([FromQuery]RecordPushDayReportRequestDto requestDto)
+    {
+        return await _recordPushDayReportService.GetRecordPushDayReportListAsync(requestDto);
+    }
+
+
+}
+
